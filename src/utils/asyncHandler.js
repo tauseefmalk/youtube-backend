5ba9860953c1b1eafe471dd6843eff2a89bfbf78
tauseefmalk <tauseefarshad23@icloud.com>
2024-12-19 16:05:15 +0500

    all setup configured

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,5 +1,5 @@
 const asyncHandler = (requestHandler) => {
-  (req, res, next) => {
+  return (req, res, next) => {
     Promise
       .resolve(requestHandler(req, res, next))
       .catch((error) => next(error));
@@ -7,6 +7,10 @@ const asyncHandler = (requestHandler) => {
 };
 
 export { asyncHandler };
+
+
+
+
 
 /*
 const asyncHandler=()=>{}
