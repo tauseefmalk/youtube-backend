6854f81fd4d67f33132b95b8dce6a1921da1eac0
tauseefmalk <tauseefarshad23@icloud.com>
2025-01-17 11:26:30 +0500

    add getWatchHistory endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -407,6 +407,59 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 });
 
+const getWatchHistory =asyncHandler(async(req, res)=>{
+  const user = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(req.user?._id),
+      }
+    },
+    {
+      $lookup:{
+        from: "videos",
+        localField: "watchHistory",
+        foreignField: "_id",
+        as: "watchHistory",
+        pipeline:[
+          {
+            $lookup:{
+              from: "users",
+              localField: "owner",
+              foreignField: "_id",
+              as: "owner",
+              pipeline:[
+                {
+                  $project:{
+                    username: 1,
+                    avatar: 1,
+                    fullName: 1
+                  }
+                }
+              ]
+            }
+          },
+          {
+            $addFields:{ //aik field add kr do
+              owner:{ // 1. Array elementAt and 2. $first
+                $first: "$owner" // getting from field that's why dollar sign
+              }
+            }
+          }
+        ]
+      }
+    }
+  ])
+
+  return res
+  .status(200)
+  .json(
+    new ApiResponse(
+      200, user[0].watchHistory,
+      "watch history fetched successfully"
+    )
+  )
+})
+
 export {
   registerUser,
   loginUser,
@@ -417,5 +470,6 @@ export {
   updateAccountDetails,
   updateUserAvatar,
   updateUserCoverImage,
-  getUserChannelProfile
+  getUserChannelProfile,
+  getWatchHistory
 };
